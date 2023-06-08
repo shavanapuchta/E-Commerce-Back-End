df551f990483df9bba52ba490aa48c8acfbb25af
shavanapuchta <119765380+shavanapuchta@users.noreply.github.com>
2023-06-07 18:08:48 -0700

    Update product.js
    added category id

--- models/product.js
+++ models/product.js
@@ -32,6 +32,11 @@ Product.init(
       validate: {
         isNumeric: true
       }
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
     }
   },
   {
