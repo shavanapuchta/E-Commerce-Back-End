517c76bbec32d5bae89c8a3ff2acdda47ed232fe
shavanapuchta <119765380+shavanapuchta@users.noreply.github.com>
2023-06-07 17:57:54 -0700

    Update tag-routes.js
    put and delete route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -23,7 +23,7 @@ router.get('/:id', async (req, res) => {
     });
 
     if (!tagData) {
-      res.status(404).json({ message: 'Tag data not found :('});
+      res.status(404).json({ message: 'Tag data not found.'});
       return;
     }
 
@@ -33,16 +33,43 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+router.post('/', async (req, res) => {
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: { id: req.params.id }
+    });
+
+    if (tagData[0] === 0) {
+      res.status(404).json({ message: 'Tag data not found.' });
+      return;
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: { id: req.params.id }
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'Tag data not found.' });
+      return;
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
